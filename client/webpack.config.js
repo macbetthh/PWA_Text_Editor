c317366de48e44350b7a0963cf7b33feeb152599
Beth McKinney <143623621+macbetthh@users.noreply.github.com>
2024-07-30 18:30:26 -0600

    adjusting webpack config to make sure my injectmanifest isn't loading multiple times. fixing icon pathing for the logo, fixing some missing dependencies on the client side with npm installs

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,12 +3,45 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
+module.exports = (env, argv) => {
+  const isProduction = argv.mode === 'production';
 
-module.exports = () => {
+  const plugins = [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      title: 'Just Another Text Editor',
+    }),
+    new WebpackPwaManifest({
+      fingerprints: false,
+      inject: true,
+      name: 'Just Another Text Editor',
+      short_name: 'JATE',
+      description: 'A simple text editor!',
+      background_color: '#ffffff',
+      theme_color: '#31a9e1',
+      start_url: './',
+      publicPath: './',
+      icons: [
+        {
+          src: path.resolve('src/images/logo.png'),
+          sizes: [96, 128, 192, 256, 384, 512],
+          destination: path.join('assets', 'icons'),
+        },
+      ],
+    }),
+  ];
+
+  if (isProduction) {
+    plugins.push(
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'service-worker.js',
+      })
+    );
+  }
+
   return {
-    mode: 'development',
+    mode: isProduction ? 'production' : 'development',
     entry: {
       main: './src/js/index.js',
       install: './src/js/install.js'
@@ -17,13 +50,24 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
-    plugins: [
-      
-    ],
-
+    plugins,
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+              plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+            },
+          },
+        },
       ],
     },
   };
